f2235f0f42a27b9f2213e7064e81543445230417
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 23:54:36 +0100

    Add functionality with useEffect for redrawin stage

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,8 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createStage } from '../helpers/helpers';
 
-export const useStage = () => {
-  const [stage, setStage] = useState(createStage());
+export const useStage = (player, resetPlayer) => {
+  const [stage, setStage] = useState(createStage(player, resetPlayer));
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // re-render empty cells
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === 'clear' ? [0, 'clear'] : cell))
+      );
+
+      //  draw the player block
+      player.block.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ];
+          }
+        });
+      });
+
+      return newStage;
+    };
+
+    setStage((prev) => updateStage(prev));
+  }, [player.collided, player.pos.x, player.pos.y, player.block]);
 
   return [stage, setStage];
 };
