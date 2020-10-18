2b373ec9a4ee69765baec2e8d20e9462abf7fae2
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 21:41:20 +0100

    Add scaffolding

---
+++ src/components/StartButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const StartButton = () => {
+  return <div></div>;
+};
---
+++ src/components/Tetris.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Tetris = () => {
+  return <div></div>;
+};
