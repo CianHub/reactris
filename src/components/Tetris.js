3f65bbf36b110a24c9b1e61d9e1c0361d7ae1cd4
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 22:31:07 +0100

    Add shading to cell

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -7,17 +7,19 @@ import { createStage } from '../helpers/helpers';
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()}></Stage>
-      <aside>
-        <div>
-          <Display text="Score"></Display>
-          <Display text="Rows"></Display>
-          <Display text="Level"></Display>
-        </div>
-        <StartButton></StartButton>
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()}></Stage>
+        <aside>
+          <div>
+            <Display text="Score"></Display>
+            <Display text="Rows"></Display>
+            <Display text="Level"></Display>
+          </div>
+          <StartButton></StartButton>
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
