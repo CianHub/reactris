9b66336f0dbc23a93b8c6a4bf122de26f8f41f20
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 23:20:56 +0100

    Add hooks to Tetris component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
-import { createStage } from '../helpers/helpers';
+
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
 
 const Tetris = () => {
+  const [stage, setStage] = useStage();
+  const [player, setPlayer] = usePlayer();
+  const [dropSpeed, setDropSpeed] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  console.log('re-render');
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()}></Stage>
+        <Stage stage={stage}></Stage>
         <aside>
           <div>
             <Display text="Score"></Display>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { randomBlock } from '../blocks';
+import { blockGenerator } from '../blocks';
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    block: randomBlock().shape,
+    block: blockGenerator().shape,
     collided: false,
   });
 
-  return player;
+  return [player, setPlayer];
 };
