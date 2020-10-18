ab72eef70593239506bfcf1fe8420cc5a7e42da6
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 22:48:01 +0100

    Add grid styling

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,11 +3,7 @@ import { StyledCell } from './styles/StyledCell';
 import { BLOCK } from '../blocks';
 
 const Cell = ({ type }) => {
-  return (
-    <StyledCell type={'L'} color={BLOCK['L'].color}>
-      Cell
-    </StyledCell>
-  );
+  return <StyledCell type={'I'} color={BLOCK['I'].color}></StyledCell>;
 };
 
 export default Cell;
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Cell from './Cell';
+import { StyledStage } from './styles/StyledStage';
 
 const Stage = ({ stage }) => {
   console.log(stage);
   return (
-    <div>
+    <StyledStage width={stage[0].length} height={stage.length}>
       {stage?.map((row) =>
         row.map((cell, x) => <Cell key={x} type={cell[0]}></Cell>)
       )}
       <Cell></Cell>
-    </div>
+    </StyledStage>
   );
 };
 
