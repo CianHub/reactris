e22594ed04474d85cd396981e8323cfd2bd9c444
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 23:31:22 +0100

    Add scaffolding for main game loop

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -7,6 +7,7 @@ import StartButton from './StartButton';
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { createStage } from '../helpers/helpers';
 
 const Tetris = () => {
   const [player, setPlayer] = usePlayer();
@@ -15,8 +16,46 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   console.log('re-render');
+
+  const movePlayer = (direction) => {
+    // Player horizontal movmement
+    updatePlayerPosition({ x: direction, y: 0 });
+  };
+
+  const startGame = () => {
+    // Reset all
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPosition({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    // When player presses down arrow block falls faster
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        // left keyboard arrow
+        movePlayer(-1);
+      }
+      if (keyCode === 39) {
+        // right keyboard arrow
+        movePlayer(1);
+      }
+      if (keyCode === 40) {
+        // down keyboard arrow
+        dropPlayer();
+      }
+    }
+  };
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage}></Stage>
         <aside>
@@ -29,7 +68,7 @@ const Tetris = () => {
               <Display text="Level"></Display>
             </div>
           )}
-          <StartButton></StartButton>
+          <StartButton onClick={() => startGame()}></StartButton>
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
