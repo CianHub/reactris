adebe14c07091c938c665a17882cf1a068298a68
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-18 22:26:13 +0100

    Create cell styled component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { BLOCK } from '../blocks';
 
 const Cell = ({ type }) => {
-  return <div>Cell</div>;
+  return (
+    <StyledCell type={'L'} color={BLOCK['L'].color}>
+      Cell
+    </StyledCell>
+  );
 };
 
 export default Cell;
